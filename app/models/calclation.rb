7208650f67fa15a64019c078c2f7fc3f71fb833b
class-Kazushi <k.a.z928marine@gmail.com>
2020-12-17 11:38:56 +0900

    Calclationテーブル作成

---
+++ app/models/calclation.rb
@@ -0,0 +1,2 @@
+class Calclation < ApplicationRecord
+end
---
+++ db/migrate/20201217022927_create_calclations.rb
@@ -0,0 +1,15 @@
+class CreateCalclations < ActiveRecord::Migration[6.0]
+  def change
+    create_table :calclations do |t|
+      t.string :name,            null: false
+      t.integer :personality_id, null: false
+      t.integer :race_num,       null: false
+      t.integer :individual_num, null: false
+      t.integer :effort_num,     null: false
+      t.integer :change_num,     null: false
+      t.integer :ability_id,     null: false
+      t.integer :tool_id,        null: false
+      t.timestamps
+    end
+  end
+end
