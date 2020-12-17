24b1482d42a86ecbf0e8ade2c89082319e5809d3
class-Kazushi <k.a.z928marine@gmail.com>
2020-12-17 11:52:43 +0900

    空欄制限のバリデーション設定

--- app/models/calclation.rb
+++ app/models/calclation.rb
@@ -1,2 +1,12 @@
 class Calclation < ApplicationRecord
+  with_options presence: true do
+    validates :name, length: { maximum: 6 }, format: { with: /\A[ァ-ヶー－]+\z/ }
+    validates :personality_id
+    validates :race_num
+    validates :individual_num
+    validates :effort_num
+    validates :change_num
+    validates :ability_id
+    validates :tool_id
+  end
 end
