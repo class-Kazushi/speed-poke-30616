34eea29fc0ca471e1fbf7efeebb8f49c7e398907
class-Kazushi <k.a.z928marine@gmail.com>
2020-12-18 19:45:52 +0900

    道具補正プルダウンの修正

--- app/models/tool.rb
+++ app/models/tool.rb
@@ -3,7 +3,8 @@ class Tool < ActiveHash::Base
     { id: 1, name: '-----' },
     { id: 2, name: 'もちもの補正なし' },
     { id: 3, name: '×1.5 (こだわりスカーフ等)' },
-    { id: 4, name: '×0.5 (くろいてっきゅう・ルームサービス等)' }
+    { id: 4, name: '×2.0 (スピードパウダー等)' },
+    { id: 5, name: '×0.5 (くろいてっきゅう・ルームサービス等)' }
   ]
 
   include ActiveHash::Associations
