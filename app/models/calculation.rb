d233f9a3b8bc487478a9bc825ebcfde31df2cdb8
class-Kazushi <k.a.z928marine@gmail.com>
2020-12-18 13:12:40 +0900

    性格補正フォーム　プルダウン表示

--- app/models/calculation.rb
+++ app/models/calculation.rb
@@ -1,7 +1,10 @@
 class Calculation < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :personality
+
   with_options presence: true do
     validates :name, length: { maximum: 6 }, format: { with: /\A[ァ-ヶー－]+\z/ }
-    validates :personality_id
+    validates :personality_id, numericality: { other_than: 1 }
     validates :race_num
     validates :individual_num
     validates :effort_num
---
+++ app/models/personality.rb
@@ -0,0 +1,12 @@
+class Personality < ActiveHash::Base
+  self.data = [
+    { id: 1, name: '---' },
+    { id: 2, name: '×1.1 (ようき・おくびょう等)' },
+    { id: 3, name: '×1.0 (性格補正なし)' },
+    { id: 4, name: '×0.9 (ゆうかん・れいせい等)' },
+  ]
+
+  include ActiveHash::Associations
+  has_many :calculations
+  
+end
